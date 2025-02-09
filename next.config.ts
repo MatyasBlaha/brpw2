ea58d4e712da257e4117b7b3713bbd151b38bdf6
MatyasBlaha <matyasblaha1@gmail.com>
2025-02-09 11:58:06 +0100

    setup chakraUi

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,25 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+import * as path from "path";
 
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
+    future: {
+      webpack5: true,
+    },
     webpack: (config) => {
         config.resolve.alias = {
             ...config.resolve.alias,
             '@': path.resolve(__dirname, 'src'),
         }
         return config
-    }
+    },
+    experimental: {
+        optimizePackageImports: ["@chakra-ui/react"],
+    },
 }
 
 export default withNextIntl(nextConfig)
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import {useTranslations} from "next-intl";
+import { useColorMode } from "@/components/ui/color-mode";
+import {Box, Button, Text} from "@chakra-ui/react";
 
 export default function Page() {
     const t = useTranslations("HomePage")
+    const { toggleColorMode } = useColorMode();
 
     return (
-        <div className='conteiner'>{t("title")}</div>
+        <Box display="flex">
+            <Text className='conteiner' color="fg.muted">{t("title")}</Text>
+            <Button bgColor="primary" onClick={toggleColorMode}>Toggle Color Mode</Button>
+        </Box>
     );
 }
