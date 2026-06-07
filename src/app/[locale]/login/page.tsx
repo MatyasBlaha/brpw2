b12d97fdf622b0a9e952abcbb30a9c9ffe3bef52
MatyasBlaha <matyasblaha1@gmail.com>
2026-06-07 15:06:00 +0200

    test

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -6,6 +6,7 @@ import {useForm, SubmitHandler} from 'react-hook-form';
 import {useMutation} from "@tanstack/react-query";
 import {apiRequest} from "@/lib/api/apiClient";
 import {Login} from "@/types/auth/login";
+import { log } from 'console';
 
 export default function AuthForm({params: {locale}}: { params: { locale: string } }) {
     const router = useRouter();
@@ -31,6 +32,7 @@ export default function AuthForm({params: {locale}}: { params: { locale: string 
     }
 
     console.log("test")
+    console.log("test2")
 
     return (
         <div className="auth-container">
